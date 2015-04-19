fefea5fb5dfd52ce4015c53d335aa23a8df81eaa
FoxDen <lisa.t.ngo@gmail.com>
2015-04-19 19:16:42 -0400

    Must create worker class tomorrow.

--- TowsonChicagoCitation/src/ChicagoCitationGUI.java
+++ TowsonChicagoCitation/src/ChicagoCitationGUI.java
@@ -6,86 +6,158 @@ import java.awt.event.ItemListener;
 import java.util.Scanner;
 
 public class ChicagoCitationGUI implements ItemListener{
-	public final static int fieldLength = 20;
+	public final static int FIELDLENGTH = 20;
+	final static String BOOK = "Book";
+	final static String CHAPTER = "Chapter In Book";
+	final static String EDITOR = "Book with Editor/Translator";
+	final static String NEWSPAPER =	"Newspaper"; 
+	final static String OFFLINEJOURNAL = "Offline Journal";
+	final static String ONLINEJOURNAL = "Online Journal";
+	final static String ENCYCLOPEDIA = "Encyclopedia";
+	final static String WEBSITE = "Website";
 		
-	JPanel citationTypes;
-	JPanel books;
-	JPanel booksWithEditors;
-	JPanel bookChapters;
+	private JPanel citationTypes;
+	private JPanel bookPanel;
+	private JPanel booksWithEditorsPanel;
+	private JPanel bookChaptersPanel;
 	
-	JPanel newspaper;
-	JPanel onlineJournal;
-	JPanel offlineJournal;
-	JPanel interviews;
-	JPanel encyclopedia;
-	JPanel website;
+	private bookClass book = new bookClass(BOOK);
+	private bookClass chapter = new bookClass(CHAPTER);
+	private bookClass editors = new bookClass(EDITOR);
+		
+	private JPanel newspaperPanel;
+	private JPanel onlineJournalPanel;
+	private JPanel offlineJournalPanel;
+	private JPanel interviewsPanel;
+	private JPanel encyclopediaPanel;
+	private JPanel websitePanel;
 	
-	JLabel author;
-	JLabel publishingCompany;
-	JLabel publishingCity;
-	JLabel pageNumber;
-	JLabel titleOfBook;
-	JLabel titleOfArticle; 
-	JLabel publicationYear;
-	JLabel URL;
+	private bookClass onJournal = new bookClass(ONLINEJOURNAL);
+	private bookClass offJournal = new bookClass(OFFLINEJOURNAL);
+	private bookClass encyclopedia = new bookClass(ENCYCLOPEDIA);
+	private bookClass website = new bookClass(WEBSITE);
+	private bookClass newspaper = new bookClass(NEWSPAPER);
+
+	
+	//private JTextField pageNo;
+	private JTextArea output;
+	private JLabel pageNumber;
+
 	
 	JCheckBox isFootnote;
+	JButton generate;
+	JButton clear;
+	
 	Scanner scanInput;
 	
+	
+	//JPanel card;
+	final static String BUTTONPANEL = "Card with JButtons";
+    final static String TEXTPANEL = "Card with JTextField";
 	public void addComponent(Container container){
-		JPanel citationTypes = new JPanel();
-		String citationItems[] = {"Book", "Chapter In Book",
-								  "Book with Editor/Translator", "Newspaper",
-								  "Online Journal", "Offline Journal",
-								  "Interview", "Encyclopedia",
-								  "Website"};
+		JPanel citationBoxPane = new JPanel();
+		JPanel bottomBit = new JPanel(new FlowLayout());
+		JPanel bB = new JPanel();
+		JPanel Bb = new JPanel(new BorderLayout());
+		
+		String citationItems[] = {BOOK, CHAPTER, EDITOR, ONLINEJOURNAL, OFFLINEJOURNAL, WEBSITE, ENCYCLOPEDIA, NEWSPAPER};
 		JComboBox<String> cb = new JComboBox<String>(citationItems);
 		cb.setEditable(false);
 		cb.addItemListener(this);
-		citationTypes.add(cb);
+		citationBoxPane.add(cb);
+
 		
-		bookComponents();
-		journalComponents();
-		otherComponents();
+		isFootnote = new JCheckBox("Is this a footnote?");
+		generate = new JButton("Generate");
+		clear = new JButton("Clear");
+		output = new JTextArea(2,30);
 		
-		citationTypes.add(onlineJournal, "Online Journal");
-		citationTypes.add(offlineJournal, "Offline Journal");
-		citationTypes.add(interviews, "Interviews");
-		citationTypes.add(encyclopedia, "Encyclopedia Entry");
-		citationTypes.add(website, "Website");
+		bottomBit.add(isFootnote);
+		bottomBit.add(generate);
+		bottomBit.add(clear);
+		bB.add(output);
+		Bb.add(bB,BorderLayout.PAGE_START);
+		Bb.add(bottomBit,BorderLayout.PAGE_END);
+
 		
-		citationTypes.add(books, "Book");
-		citationTypes.add(bookChapters, "Chapter In Book");
-		citationTypes.add(booksWithEditors, "Books with Editors");
+		booksWithEditorsPanel = editors.returnJPanel(CHAPTER);
+		bookChaptersPanel = chapter.returnJPanel(EDITOR);
+		onlineJournalPanel = onJournal.returnJPanel(ONLINEJOURNAL);
+		offlineJournalPanel = offJournal.returnJPanel(OFFLINEJOURNAL);
+		encyclopediaPanel = encyclopedia.returnJPanel(ENCYCLOPEDIA);
+		websitePanel = website.returnJPanel(WEBSITE);
+		newspaperPanel = newspaper.returnJPanel(NEWSPAPER);
+		bookPanel = book.returnJPanel(BOOK);
 		
-		container.add(citationTypes, BorderLayout.PAGE_START);
+
+		citationTypes = new JPanel(new CardLayout());
+		citationTypes.add(onlineJournalPanel, ONLINEJOURNAL);
+		citationTypes.add(offlineJournalPanel, OFFLINEJOURNAL);
+		citationTypes.add(encyclopediaPanel, ENCYCLOPEDIA);
+		citationTypes.add(websitePanel, WEBSITE);
+		citationTypes.add(bookPanel, BOOK);
+		citationTypes.add(bookChaptersPanel, CHAPTER);
+		citationTypes.add(booksWithEditorsPanel, EDITOR);
+		citationTypes.add(newspaperPanel, NEWSPAPER);
+
+		
+		container.add(citationBoxPane, BorderLayout.PAGE_START);
 		container.add(citationTypes, BorderLayout.CENTER);
+		container.add(Bb, BorderLayout.PAGE_END);
 
 	}
 	
-	public void bookComponents(){
-		SpringLayout spring = new SpringLayout();
+	public void outputCitations(){
 		
 	}
 	
-	public void journalComponents(){
-		SpringLayout spring = new SpringLayout();
-		
-	}
-	
-	public void otherComponents(){
-		SpringLayout spring = new SpringLayout();
-		
-	}
-	
-	public void changeItemState(ItemEvent event){
-		CardLayout cl = (CardLayout)(citationTypes.getLayout());
-		//cl.show(citationTypes, (String).event.getItem());
-	}
-	
+
 	@Override
-	public void itemStateChanged(ItemEvent arg0) {
-		// TODO Auto-generated method stub
+	public void itemStateChanged(ItemEvent event) {
+		CardLayout cl = (CardLayout)(citationTypes.getLayout());
+		cl.show(citationTypes, (String)event.getItem());
 		
 	}
+	
+	   private static void createAndShowGUI() {
+	        //Create and set up the window.
+	        JFrame frame = new JFrame("Bibliography Demo");
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        Dimension d = new Dimension(600,300);
+	        frame.getContentPane().setPreferredSize(d);
+	         
+	        //Create and set up the content pane.
+	        ChicagoCitationGUI demo = new ChicagoCitationGUI();
+	        demo.addComponent(frame.getContentPane());
+	         
+	        //Display the window.
+	        frame.pack();
+	        frame.setVisible(true);
+	    }
+	     
+	    public static void main(String[] args) {
+	        /* Use an appropriate Look and Feel */
+	        try {
+	            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+	            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+	        } catch (UnsupportedLookAndFeelException ex) {
+	            ex.printStackTrace();
+	        } catch (IllegalAccessException ex) {
+	            ex.printStackTrace();
+	        } catch (InstantiationException ex) {
+	            ex.printStackTrace();
+	        } catch (ClassNotFoundException ex) {
+	            ex.printStackTrace();
+	        }
+	        /* Turn off metal's use of bold fonts */
+	        UIManager.put("swing.boldMetal", Boolean.FALSE);
+	         
+	        //Schedule a job for the event dispatch thread:
+	        //creating and showing this application's GUI.
+	        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+	            public void run() {
+	                createAndShowGUI();
+	            }
+	        });
+	    }
 }
--- TowsonChicagoCitation/src/bookClass.java
+++ TowsonChicagoCitation/src/bookClass.java
@@ -1,8 +1,17 @@
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
+/*
+ ***************************TO DO***************************
+ *add tooltips
+ *Create the worker class
+ *add comments
+ ***********************************************************
+ */
 
 public class bookClass {
 	
@@ -17,9 +26,13 @@ public class bookClass {
 	private JLabel editorOrTranslator;
 	private JLabel journalVolume;
 	private JLabel journalNumber;
+	private JLabel date;
+	private JLabel year;
+	private JLabel city;
 	
-	private String type;	
-	JPanel bookBase;
+	private final String type;	
+	private String storage = "";
+	JPanel bookBase = new JPanel();
 
 	public JTextField URLText;
 	public JTextField fName;
@@ -30,6 +43,7 @@ public class bookClass {
 	public JTextField publisherName;
 	public JTextField publishCity;
 	public JTextField publishYear;
+	public JTextField publishDate;
 	public JTextField editorTranslator;
 	public JTextField pageNum;
 	public JTextField journalV;
@@ -37,78 +51,125 @@ public class bookClass {
 	
 	SpringLayout spring = new SpringLayout();
 
-	
-	public bookClass(String type) {
-		type = this.type;
+	public bookClass(String t) {
+		type = t;
 	}
 	
 	public void bookComponents(){
 		
 		bookBase = new JPanel();
 		bookBase.setLayout(spring);		
+		
 		author = new JLabel("Author");
-		fName = new JTextField("",FIELDLENGTH);
-		lName = new JTextField("",FIELDLENGTH);
-		mInit = new JTextField("",1);
-		
+		fName = new JTextField("a",FIELDLENGTH);
+		lName = new JTextField("b",FIELDLENGTH);
+		mInit = new JTextField("c",1);
 		titleOfBook = new JLabel("Title");
-		title = new JTextField("",FIELDLENGTH);
+		year = new JLabel("Year");
+		publishYear = new JTextField("1999", 4);
 		
-		publisher = new JLabel("Publisher");
-		
-		publishCity = new JTextField("",FIELDLENGTH);
-		publisherName = new JTextField("",FIELDLENGTH);
-		publishYear = new JTextField("",FIELDLENGTH);
-		
-		titleOfArticle = new JLabel("Article/Chapter Title");
-		articleTitle = new JTextField("",FIELDLENGTH);
-		URLText = new JTextField("",FIELDLENGTH);
+		title = new JTextField(type,FIELDLENGTH);
+		publisherName = new JTextField("e",FIELDLENGTH);
+		URLText = new JTextField("h",FIELDLENGTH);
 		URL = new JLabel("URL");
-		pageNum = new JTextField("", 5);
-		
-		
+		pageNum = new JTextField("i", 5);
+
+				
 		bookBase.add(fName);
 		bookBase.add(lName);
 		bookBase.add(mInit);
-		bookBase.add(publisherName);
-		bookBase.add(publishCity);
-		bookBase.add(publishYear);
-		bookBase.add(title);
-		
 		bookBase.add(author);
-		bookBase.add(publisher);
 		bookBase.add(titleOfBook);
-		bookBase.add(pageNum);
+		bookBase.add(title);
+		bookBase.add(publishYear);
+		bookBase.add(year);
+		
+		titleOfArticle = new JLabel("Chapter");
+		articleTitle = new JTextField("Article",FIELDLENGTH);
 		
 		if(type == "Online Journal" || type == "Offline Journal"){
 			journalVolume = new JLabel("Vol.");
+			titleOfBook.setText("Journal");
 			journalNumber = new JLabel("Number");
-			journalV = new JTextField("", 3);
-			journalN = new JTextField("", 3);
-			
+			journalV = new JTextField("Vol", 3);
+			journalN = new JTextField("Num", 3);
+			date = new JLabel("Month/Season published");
+			publishDate = new JTextField("Summer",10);
+			titleOfArticle.setText("Article");
+		
 			bookBase.add(titleOfArticle);
 			bookBase.add(articleTitle);
+			bookBase.add(journalVolume);
+			bookBase.add(journalNumber);
+			bookBase.add(journalV);
+			bookBase.add(journalN);
+			bookBase.add(date);
+			bookBase.add(publishDate);
 			
+			if(type == "Online Journal"){
+				bookBase.add(URL);
+				bookBase.add(URLText);
+				
+				storage+="u ";
+			}
+			storage+="a v d p ";
+
+		} 
+		
+		if(type == "Chapter In Book" || type == "Book" || type == "Book with Editor/Translator" || type == "Encyclopedia"){
+			publisher = new JLabel("Publisher");
+			city = new JLabel("City");
+			publishCity = new JTextField("City", FIELDLENGTH);
+			publisherName = new JTextField("", FIELDLENGTH);
+			bookBase.add(publisherName);
+			bookBase.add(publishCity);
+			bookBase.add(publisher);
+			bookBase.add(city);
+			storage+="b ";
+			if(type == "Book with Editor/Translator"){
+				editorOrTranslator = new JLabel("Editor/Translator");
+				editorTranslator = new JTextField("",FIELDLENGTH);
+				
+				bookBase.add(editorOrTranslator);
+				bookBase.add(editorTranslator);
+				
+				storage+="e ";
+			} 
+			
+			if (type == "Chapter In Book"){
+				bookBase.add(titleOfArticle);
+				articleTitle = new JTextField("g",FIELDLENGTH);
+				bookBase.add(articleTitle);
+				
+				storage+="a ";
+			}
+			
+			if (type == "Encyclopedia"){
+				titleOfBook.setText("Encyclopedia");
+				
+				storage+="a ";
+			}
+		} 
+		
+		if (type == "Newspaper" || type == "Website"){
 			bookBase.add(URL);
 			bookBase.add(URLText);
-
-		} else if(type == "Chapter"){
-			
 			bookBase.add(titleOfArticle);
-
-		} else if(type == "Editor"){
-			editorOrTranslator = new JLabel("Editor Or Translator");
-			editorTranslator = new JTextField("",FIELDLENGTH);
+			titleOfArticle.setText("Article");
+			articleTitle = new JTextField("g",FIELDLENGTH);
+			bookBase.add(articleTitle);
 			
-			bookBase.add(editorOrTranslator);
-			bookBase.add(editorTranslator);
+			titleOfBook.setText((type=="Newspaper")? "Newspaper":"Website");
 			
-		} 
+			publishDate = new JTextField("",10);
+			date = new JLabel("Published On");
+			
+			storage+="u d a ";
+		}
 	}
 	
-	
-	public void bookBaseSetUp(String s){
-			
+	public void bookBaseSetUp(){
+		//sets up things in general
 		spring.putConstraint(SpringLayout.SOUTH, fName, 20, SpringLayout.SOUTH, author);
 		spring.putConstraint(SpringLayout.WEST, fName, 10, SpringLayout.WEST, author);
 
@@ -118,39 +179,84 @@ public class bookClass {
 		spring.putConstraint(SpringLayout.SOUTH, mInit, 20, SpringLayout.SOUTH, author);
 		spring.putConstraint(SpringLayout.WEST, mInit, 180, SpringLayout.WEST, lName);
 
-
-		spring.putConstraint(SpringLayout.SOUTH, publisher, 50, SpringLayout.SOUTH, author);
-		spring.putConstraint(SpringLayout.WEST, publisherName, 10, SpringLayout.WEST, publisher);
-		spring.putConstraint(SpringLayout.SOUTH, publisherName, 20, SpringLayout.SOUTH, publisher);
+		spring.putConstraint(SpringLayout.SOUTH, titleOfBook, 60, SpringLayout.WEST, author);
+		spring.putConstraint(SpringLayout.SOUTH, title, 65, SpringLayout.SOUTH, author);
+		spring.putConstraint(SpringLayout.WEST, title, 10, SpringLayout.WEST, author);
 		
-		spring.putConstraint(SpringLayout.SOUTH, publishCity, 20, SpringLayout.SOUTH, publisher);
-		spring.putConstraint(SpringLayout.WEST, publishCity, 190, SpringLayout.WEST, publisher);
+		spring.putConstraint(SpringLayout.SOUTH, year, 0, SpringLayout.SOUTH, author);
+		spring.putConstraint(SpringLayout.WEST, year, 400, SpringLayout.WEST, author);		
+		spring.putConstraint(SpringLayout.SOUTH, publishYear, 20, SpringLayout.SOUTH, author);
+		spring.putConstraint(SpringLayout.WEST, publishYear, 400, SpringLayout.WEST, author);
 
-		spring.putConstraint(SpringLayout.SOUTH, publishYear, 20, SpringLayout.SOUTH, publisher);
-		spring.putConstraint(SpringLayout.WEST, publishYear, 370, SpringLayout.WEST, publisher);
-		
-		
-		spring.putConstraint(SpringLayout.SOUTH, titleOfBook, 50, SpringLayout.SOUTH, publisher);
-		spring.putConstraint(SpringLayout.SOUTH, title, 20, SpringLayout.SOUTH, titleOfBook);
-		spring.putConstraint(SpringLayout.WEST, title, 10, SpringLayout.WEST, titleOfBook);
 
-		spring.putConstraint(SpringLayout.SOUTH, pageNum, 60, SpringLayout.SOUTH, titleOfBook);
-		spring.putConstraint(SpringLayout.WEST, pageNum, 10, SpringLayout.WEST, titleOfBook);
-		
-		if(s=="Chapter"){
-			spring.putConstraint(SpringLayout.SOUTH, titleOfArticle, 120, SpringLayout.SOUTH, author);
+		if(storage.contains("b")){ //sets up book component
+			spring.putConstraint(SpringLayout.SOUTH, publisher, 60, SpringLayout.WEST, author);
+			spring.putConstraint(SpringLayout.WEST, publisher, 190, SpringLayout.WEST, author);
+			spring.putConstraint(SpringLayout.SOUTH, publisherName, 65, SpringLayout.SOUTH, author);
+			spring.putConstraint(SpringLayout.WEST, publisherName, 190, SpringLayout.WEST, author);
+			
 
-		} else if(s== "Online Journal" || s == "Offline Journal"){
-			spring.putConstraint(SpringLayout.SOUTH, c1, pad, e2, c2);
+			spring.putConstraint(SpringLayout.SOUTH, city, 60, SpringLayout.WEST, author);
+			spring.putConstraint(SpringLayout.WEST, city, 370, SpringLayout.WEST, author);
+			spring.putConstraint(SpringLayout.SOUTH, publishCity, 65, SpringLayout.SOUTH, author);
+			spring.putConstraint(SpringLayout.WEST, publishCity, 370, SpringLayout.WEST, author);
+
+		}  
+		
+
+		if(storage.contains("v")){ //sets up volume and number component
+		
+			spring.putConstraint(SpringLayout.SOUTH, journalVolume, 60, SpringLayout.WEST, author);
+			spring.putConstraint(SpringLayout.WEST, journalVolume, 190, SpringLayout.WEST, author);
+			spring.putConstraint(SpringLayout.SOUTH, journalV, 65, SpringLayout.SOUTH, author);
+			spring.putConstraint(SpringLayout.WEST, journalV, 190, SpringLayout.WEST, author);
+			
+			spring.putConstraint(SpringLayout.SOUTH, journalNumber, 50, SpringLayout.WEST, title);
+			spring.putConstraint(SpringLayout.WEST, journalNumber, 240, SpringLayout.WEST, author);
+			spring.putConstraint(SpringLayout.SOUTH, journalN, 65, SpringLayout.SOUTH, author);
+			spring.putConstraint(SpringLayout.WEST, journalN, 240, SpringLayout.WEST, author);
+			
+			//setUpPageNum();
+		} 
+		
+		if (storage.contains("a")){ //sets up article title component
+			spring.putConstraint(SpringLayout.SOUTH, titleOfArticle, 105, SpringLayout.WEST, author);
+			spring.putConstraint(SpringLayout.SOUTH, articleTitle, 125, SpringLayout.WEST, author);
+			spring.putConstraint(SpringLayout.WEST, articleTitle, 10, SpringLayout.WEST, author);
+		}
+		
+		if (storage.contains("d")){ //sets up date component
+			spring.putConstraint(SpringLayout.SOUTH, date, 60, SpringLayout.WEST, author);
+			spring.putConstraint(SpringLayout.WEST, date, 300, SpringLayout.WEST, author);
+			spring.putConstraint(SpringLayout.SOUTH, publishDate, 65, SpringLayout.SOUTH, author);
+			spring.putConstraint(SpringLayout.WEST, publishDate, 300, SpringLayout.WEST, author);
+		}
+		
+		if (storage.contains("e")){ //sets up editor component
+
+			spring.putConstraint(SpringLayout.SOUTH, editorOrTranslator, 105, SpringLayout.WEST, author);
+			spring.putConstraint(SpringLayout.SOUTH, editorTranslator, 125, SpringLayout.WEST, author);
+			spring.putConstraint(SpringLayout.WEST, editorTranslator, 10, SpringLayout.WEST, author);
+		}
+		
+		if (storage.contains("u")){ //sets up URL component
+			spring.putConstraint(SpringLayout.SOUTH, URL, 150, SpringLayout.WEST, author);
+			spring.putConstraint(SpringLayout.SOUTH, URLText, 170, SpringLayout.WEST, author);
+			spring.putConstraint(SpringLayout.WEST, URLText, 10, SpringLayout.WEST, author);
 		}
 	}
-	
-	
-	public JPanel returnJPanel(){
-		bookComponents();
 		
-		bookBaseSetUp(type);
-		return bookBase;
-		
+	public void setUppageNum(){
+		bookBase.add(pageNum);
+		bookBase.add(new JLabel("Page Numbers"));
+		spring.putConstraint(SpringLayout.SOUTH, pageNum, 155, SpringLayout.SOUTH, author);
+		spring.putConstraint(SpringLayout.WEST, pageNum, 300, SpringLayout.WEST, author);
 	}
+	
+	public JPanel returnJPanel(String s){
+		bookComponents();
+		bookBaseSetUp();
+		return bookBase;
+	}
+	
 }
